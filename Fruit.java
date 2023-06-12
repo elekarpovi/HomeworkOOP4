0a963ba9c3bbfbcca1e84d9e217884b15e601d46
Elena Karpovich <elekarpovi@tut.by>
2023-06-12 23:23:18 +0300

    Added Fruit

---
+++ Fruit.java
@@ -0,0 +1,19 @@
+public abstract class Fruit {
+
+    private final String name;
+    private final float weight;
+
+    public float getWeight() { 
+        return weight; 
+    }
+
+    public String getName() { 
+        return name; 
+    }
+
+    public Fruit(float weight, String name) {
+        this.weight = weight;
+        this.name = name;
+    }
+}
+
